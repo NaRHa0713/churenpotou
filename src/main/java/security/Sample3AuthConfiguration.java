a79b4f85645bb801cf2e29da57a5afed174a1dfe
NaRHa0713 <e1b19104@oit.ac.jp>
2021-10-19 14:58:34 +0900

    初期設定した

---
+++ src/main/java/security/Sample3AuthConfiguration.java
@@ -0,0 +1,52 @@
+package oit.is.syumai.churenpoutou.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class Sample3AuthConfiguration extends WebSecurityConfigurerAdapter {
+
+  /**
+   * 認証処理に関する設定（誰がログインできるか）
+   */
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    // 平文のパスワードをエンコーダにかけてハッシュ化し，"user1"と関連付けている．ロール名は"USER"
+    // プログラム中に素のパスワードが含まれることになるので望ましくない
+    auth.inMemoryAuthentication().withUser("user").password(passwordEncoder().encode("yakuman")).roles("USER");
+
+    // $ sshrun htpasswd -nbBC 10 admin adm1n
+    // htpasswdでBCryptエンコードを行った後の文字列をパスワードとして指定している．
+    auth.inMemoryAuthentication().withUser("admin")
+        .password("$2y$10$wnHsj/h5T47OuOGMUZdxrujGwqTuIpTnDFA2chyZisyblso1Red3K").roles("ADMIN");
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.formLogin();
+    http.authorizeRequests().antMatchers("/sample4/**").authenticated();
+
+    http.logout().logoutSuccessUrl("/");
+
+    /**
+     * 以下2行はh2-consoleを利用するための設定なので，開発が完了したらコメントアウトすることが望ましい
+     * CSRFがONになっているとフォームが対応していないためアクセスできない
+     * HTTPヘッダのX-Frame-OptionsがDENYになるとiframeでlocalhostでのアプリが使えなくなるので，H2DBのWebクライアントのためだけにdisableにする必要がある
+     */
+    http.csrf().disable();
+    http.headers().frameOptions().disable();
+  }
+}
