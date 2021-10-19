f0c1214267d0fc464d3e212b3c5c16c1a9904874
NaRHa0713 <e1b19104@oit.ac.jp>
2021-10-19 14:26:04 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/syumai/churenpoutou/ChurenpoutouApplication.java
@@ -0,0 +1,13 @@
+package oit.is.syumai.churenpoutou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurenpoutouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChurenpoutouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/syumai/churenpoutou/ChurenpoutouApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.syumai.churenpoutou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurenpoutouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
